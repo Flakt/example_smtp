620eb136ade85462272661c9b65e9a58a6eb1cdc
Stefan Chan <chanstefan98@gmail.com>
2021-07-13 15:00:08 +0200

    Initial smtp client

---
+++ example_smtp.py
@@ -0,0 +1,52 @@
+from objects.SocketObject import SocketObject
+
+import sys
+
+## TODO: Replace string inserts with parameterized inserts so that SMTP injections
+##       does not work
+
+##       Consider a separate function which handles sending and reading return codes
+
+HOST_IP = "127.0.0.1"
+HOST_PORT = 5001
+DOMAIN = "example.com"
+SENDER = "sender@test.com"
+RECIPIENT = "recipient@test.com"
+
+def send_smtp_mail():
+    clientSocket = SocketObject(HOST_IP, HOST_PORT)
+
+    conn_resp = clientSocket.connect()
+    if conn_resp[:3] != '220':
+        error_code('220')
+
+    helo_resp = clientSocket.send_command("HELO " + DOMAIN + "\r\n")
+    if helo_resp[:3] != '250':
+        error_code('250')
+
+    mail_from_resp = clientSocket.send_command("MAIL FROM:" + SENDER + "\r\n")
+    if mail_from_resp[:3] != '250':
+        error_code('250')
+
+    clientSocket.send_no_ret_command("RCPT TO:" + RECIPIENT + "\r\n")
+
+    data_resp = clientSocket.send_command("DATA\r\n")
+    if data_resp[:3] != '354':
+        error_code('354')
+    
+    content_type = "Content-Type: text/html;\n"
+    smtp_headers = "From: " + SENDER + "\nTo: " + RECIPIENT + "\nSubject: Happy birthday :D\n\n"
+    content = "I hope you've hade a wonderful day!"
+    ending_dot = "\r\n.\r\n"
+    message_command = content_type + smtp_headers + content + ending_dot
+
+    mesg_resp = clientSocket.send_command(message_command)
+    if mesg_resp[:3] != '250':
+        error_code('250')
+
+    clientSocket.close_connection()
+
+
+def error_code(code):
+    print(code + " not replied from SMTP-Server")
+    sys.exit(1)
---
+++ objects/SocketObject.py
@@ -0,0 +1,29 @@
+from dataclasses import dataclass
+from socket import socket, AF_INET, SOCK_STREAM
+
+@dataclass
+class SocketObject:
+    def __init__(self, addr: str, port: int):
+        self.smtp_socket = socket(AF_INET, SOCK_STREAM)
+        self.addr = addr
+        self.port = port
+
+    def connect(self) -> str:
+        self.smtp_socket.connect((self.addr, self.port))
+        ret_mesg = self.smtp_socket.recv(1024).decode()
+        
+        return ret_mesg
+
+    def send_command(self, command) -> str:
+        self.smtp_socket.send(command.encode())
+        ret_mesg = self.smtp_socket.recv(1024).decode()
+
+        return ret_mesg
+
+    def send_no_ret_command(self, command):
+        self.smtp_socket.send(command.encode())
+
+    def close_connection(self):
+        quit_command = "QUIT\r\n".encode()
+        self.smtp_socket.send(quit_command)
+
